1a2cb202cc633ff4dbc3938ab4d9e642dd0e43aa
bziolo-sedric <bartosz.s.ziolo@sedric.ai>
2025-07-16 15:03:44 +0200

    Fix release attempt 1

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 import setuptools
+import subprocess
+
+def get_git_tag():
+    try:
+        return subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"]).decode().strip()
+    except Exception:
+        return "0.0.0"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mock-firestore-plus",
-    version="0.12.3",
+    name="mock-firestore-sharp",
+    version=get_git_tag(),
     author="Matt Dowds",
     description="In-memory implementation of Google Cloud Firestore for use in tests",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/eddie/python-mock-firestore",
+    url="https://github.com/bziolo-sedric/python-mock-firestore",
     packages=setuptools.find_packages(),
     test_suite='',
     classifiers=[
